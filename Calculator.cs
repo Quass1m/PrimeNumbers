690a1d4a04067ee76fb47a47a21f8c3e5c80036d
Konrad Pietralik <konrad.pietralik@gmail.com>
2017-05-07 22:01:13 +0200

    New implementation - using IEnumerable.Range()

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace PrimeNumbers
 {
@@ -12,18 +14,39 @@ namespace PrimeNumbers
             Console.WriteLine($"Calculate prime numbers up to {maxNumber}");
             var watch = new Stopwatch();
 
+            Func<long, bool> foo = number => 
+            {
+                if (number == 0)
+                    return false;
+
+                for (long i = 2; i <= number / 2; i++)
+                {
+                    if (number % i == 0)
+                        return false;
+                }
+
+                return true;
+            };
+
             watch.Start();
             using (var stream = new FileStream(filePath,FileMode.Create))
             {
-                for (long i = 2; i <= maxNumber; i++)
-                {
-                    var perNumberTime = new Stopwatch();
-                    perNumberTime.Start();
-                    bool isPrime = IsPrime(i);
-                    perNumberTime.Stop();
+                //for (long i = 2; i <= maxNumber; i++)
+                //{
+                //    //var perNumberTime = new Stopwatch();
+                //    //perNumberTime.Start();
+                //    //bool isPrime = IsPrime(i);
+                //    //perNumberTime.Stop();
 
-                    if (isPrime)
-                        AddNumber(stream, i, perNumberTime.ElapsedTicks);
+                //    if (isPrime)
+                //        AddNumber(stream, i, 0);
+                //}
+
+                IEnumerable<int> range = Enumerable.Range(2, (int)maxNumber);
+
+                foreach(int i in range.Where(n => foo(n)))
+                {
+                    AddNumber(stream, i, 0);
                 }
             }
             watch.Stop();
